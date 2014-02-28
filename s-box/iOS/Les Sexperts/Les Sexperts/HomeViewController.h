17104d042aa0a32a02987499b385cd45c7b03ff3
Paul De Lange <pauldelange@iMac-de-Denis.local>
2014-02-28 08:51:52 +0100

    Project start

---
+++ s-box/iOS/Les Sexperts/Les Sexperts/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.h
+//  Les Sexperts
+//
+//  Created by Paul De Lange on 28/02/14.
+//  Copyright (c) 2014 Gilmert Bentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+- (IBAction)menuPushed:(id)sender;
+
+@end
---
+++ s-box/iOS/Les Sexperts/Les Sexperts/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  Les Sexperts
+//
+//  Created by Paul De Lange on 28/02/14.
+//  Copyright (c) 2014 Gilmert Bentley. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+#pragma mark - Actions
+- (IBAction)menuPushed:(id)sender {
+
+}
+
+#pragma mark - UIViewController
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+@end
