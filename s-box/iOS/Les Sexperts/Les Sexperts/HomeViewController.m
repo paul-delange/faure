56df38412506ea2efd96b9d051438cfd4d86b2d1
Paul de Lange <pauldelange@pauls-mbp.home>
2014-03-14 22:38:06 +1000

    home change

--- s-box/iOS/Les Sexperts/Les Sexperts/HomeViewController.m
+++ s-box/iOS/Les Sexperts/Les Sexperts/HomeViewController.m
@@ -10,7 +10,11 @@
 
 #import "Joke.h"
 
-@interface HomeViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface HomeViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+@property (weak, nonatomic) IBOutlet UIButton *adviceButton;
+@property (weak, nonatomic) IBOutlet UIButton *jokeButton;
 
 @end
 
@@ -39,54 +43,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
-    UITableView* tableView = [[UITableView alloc] initWithFrame: self.view.bounds];
-    tableView.delegate = self;
-    tableView.dataSource = self;
-    tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    [self.view addSubview: tableView];
-    [tableView reloadData];
-}
-
-#pragma mark - UITableViewDataSource
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault reuseIdentifier: nil];
-    switch (indexPath.row) {
-        case 0:
-            cell.textLabel.text = @"Jouer";
-            break;
-        case 1:
-            cell.textLabel.text = @"Conseils";
-            break;
-        case 2:
-            cell.textLabel.text = @"Blagues";
-            break;
-        default:
-            break;
-    }
-    return cell;
-}
-
-#pragma mark - UITableViewDelegate
-- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell* cell = [tableView cellForRowAtIndexPath: indexPath];
-    switch (indexPath.row) {
-        case 0:
-            [self performSegueWithIdentifier: @"GamePushSegue" sender: cell];
-            break;
-        case 1:
-            [self performSegueWithIdentifier: @"ThemePushSegue" sender: cell];
-            break;
-            
-        case 2:
-            [self performSegueWithIdentifier: @"JokePushSegue" sender: cell];
-            break;
-        default:
-            break;
-    }
 }
 
 @end
