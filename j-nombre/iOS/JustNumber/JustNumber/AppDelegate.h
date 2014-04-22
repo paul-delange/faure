1552ab3d4c5cdc350e5de98323be887e886c6bd5
Paul de Lange <pauldelange@Pauls-MacBook-Pro.local>
2014-04-22 20:11:56 +1000

    ios project

---
+++ j-nombre/iOS/JustNumber/JustNumber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JustNumber
+//
+//  Created by Paul de Lange on 22/04/2014.
+//  Copyright (c) 2014 Gilmert Bentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
